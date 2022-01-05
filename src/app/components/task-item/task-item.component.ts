c5063d9665c2bf73a1ce0c6f9f5441ffac8eca58
Sigit <sigittt53@gmail.com>
2022-01-05 11:40:14 +0700

    initial and create todo

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+
+  @Input() task: Task = {
+    id: 0,
+    text: '',
+    day: '',
+    reminder: false,
+  }
+
+  faTimes = faTimes
+
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleTask: EventEmitter<Task> = new EventEmitter()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task)
+  }
+
+  onToggle(task: Task) {
+    this.onToggleTask.emit(task)
+  }
+
+}
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core'
+import { Task } from '../../Task'
+import { TaskService } from '../../services/task.service'
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = []
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
+  }
+
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => {
+      return this.tasks = this.tasks.filter(oldTask => oldTask.id !== task.id)
+    })
+  }
+
+  toggleTask(task: Task) {
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((e) => {
+      return this.tasks = [e, ...this.tasks]
+    })
+  }
+
+}
